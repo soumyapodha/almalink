cb06938324c2700d03820181ac4f58d5863afbb1
SoumyaPodha <67249492+soumyapodha@users.noreply.github.com>
2022-11-20 22:10:02 +0530

    V 2.0

---
+++ main.js
@@ -0,0 +1,12 @@
+const header = document.querySelector("header");
+const hambugerMenu = document.querySelector(".hamburger-menu");
+
+hambugerMenu.addEventListener("click", () => {
+  header.classList.toggle("active");
+});
+
+window.addEventListener("scroll", () => {
+  document.body.scrollTop > 20 || document.documentElement.scrollTop > 20
+    ? header.classList.add("sticky")
+    : header.classList.remove("sticky");
+});
