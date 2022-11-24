e110eda48e0ff7742bda9b74d5e7a43ef8d0c3d9
Coder-sam-007 <padamzain178@gmail.com>
2022-11-24 22:20:20 +0530

    contact section added

--- main.js
+++ main.js
@@ -10,3 +10,22 @@ window.addEventListener("scroll", () => {
     ? header.classList.add("sticky")
     : header.classList.remove("sticky");
 });
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
