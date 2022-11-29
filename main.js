0462d103f63c8ba5ef11fe103c8885d97ceb0a11
Soumya <manishkupodha@gmail.com>
2022-11-30 01:51:14 +0530

    sign in added

--- main.js
+++ main.js
@@ -34,6 +34,30 @@ inputs.forEach((input) => {
   input.addEventListener("blur", blurFunc);
 });
 
+
+// sign in js
+
+const open_modal = document.querySelector("#sign-in");
+const containo= document.querySelector(".containo");
+const submit_btn = document.querySelector("#submit-btn");
+const close_btn = document.querySelector(".close-btn");
+
+open_modal.addEventListener("click",(e)=>{
+  e.preventDefault();
+containo.style.display = 'block';
+document.body.style.height = '100vh';
+});
+
+submit_btn.addEventListener("click",(e)=>{
+  e.preventDefault();
+  containo.style.display = 'none';
+  });
+
+  close_btn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    containo.style.display = 'none';
+    });
+
 /* QUIZZES */
 
 // const quizData = [
@@ -105,7 +129,7 @@ inputs.forEach((input) => {
 //   d_text.innerText = currentQuizData.d;
 // };
 
-// loadQuiz(quizData);
+// loadQuiz();
 
 // function getSelected() {
 //   let answer;
@@ -135,113 +159,4 @@ inputs.forEach((input) => {
 //   }
 // });
 
-const quizData = [
-  {
-    question: "Which language runs in a web browser?",
-    a: "Java",
-    b: "C",
-    c: "Python",
-    d: "javascript",
-    correct: "d",
-  },
-  {
-    question: "What does CSS stand for?",
-    a: "Central Style Sheets",
-    b: "Cascading Style Sheets",
-    c: "Cascading Simple Sheets",
-    d: "Cars SUVs Sailboats",
-    correct: "b",
-  },
-  {
-    question: "What does HTML stand for?",
-    a: "Hypertext Markup Language",
-    b: "Hypertext Markdown Language",
-    c: "Hyperloop Machine Language",
-    d: "Helicopters Terminals Motorboats Lamborginis",
-    correct: "a",
-  },
-  {
-    question: "What year was JavaScript launched?",
-    a: "1996",
-    b: "1995",
-    c: "1994",
-    d: "none of the above",
-    correct: "b",
-  },
-];
-
-const quiz = document.querySelector(".quiz-header");
-const box = document.querySelectorAll(".box");
-
-const answerEls = document.querySelectorAll(".answer");
-const questionEl = document.getElementById("question");
-const a_text = document.getElementById("a_text");
-const b_text = document.getElementById("b_text");
-const c_text = document.getElementById("c_text");
-const d_text = document.getElementById("d_text");
-const submitBtn = document.getElementById("submit");
-
-for (ind = 0; ind < box.length; ind++) {
-  box[ind].id = "" + ind;
-}
-box.forEach((e) => {
-  e.addEventListener("click", () => {
-    quiz.style.display = "block";
-    currentQuiz = parseInt(e.id);
-    deselectAnswers();
-    console.log(currentQuiz);
-    const currentQuizData = quizData[currentQuiz];
-    questionEl.innerText = currentQuizData.question;
-    a_text.innerText = currentQuizData.a;
-    b_text.innerText = currentQuizData.b;
-    c_text.innerText = currentQuizData.c;
-    d_text.innerText = currentQuizData.d;
-  });
-});
-
-let currentQuiz = 0;
-let score = 0;
-
-function deselectAnswers() {
-  answerEls.forEach((answerEl) => (answerEl.checked = false));
-}
-
-const loadQuiz = () => {
-  deselectAnswers();
-  const currentQuizData = quizData[currentQuiz];
-  questionEl.innerText = currentQuizData.question;
-  a_text.innerText = currentQuizData.a;
-  b_text.innerText = currentQuizData.b;
-  c_text.innerText = currentQuizData.c;
-  d_text.innerText = currentQuizData.d;
-};
-
-loadQuiz();
-
-function getSelected() {
-  let answer;
-  answerEls.forEach((answerEl) => {
-    if (answerEl.checked) {
-      answer = answerEl.id;
-    }
-  });
-  return answer;
-}
-
-submitBtn.addEventListener("click", () => {
-  const answer = getSelected();
-  if (answer) {
-    if (answer === quizData[currentQuiz].correct) {
-      score++;
-    }
-    currentQuiz++;
-    if (currentQuiz < quizData.length) {
-      loadQuiz();
-    } else {
-      quiz.innerHTML = `
-           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
-           <button onclick="location.reload()">Try Again</button>
-           `;
-    }
-  }
-});
+// 
